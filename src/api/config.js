bbe99c09da7a175830e1caa7dd8f272fc5c53cad
drlanah <lanahuhko@gmail.com>
2020-09-12 17:17:41 -0400

    api setup

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,7 @@
-// export const apiKey = "###";
+require("dotenv").config();
+
+export default yelpApiKey({
+  apiKey: process.env.REACT_APP_API_KEY_YELP,
+})(MapContainer);
+
+// export const apiKey = "";
